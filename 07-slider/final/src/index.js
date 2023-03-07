8644856886aa9546360e580c7d1bd2f9d5a9427f
smajaas <smajaas@gmail.com>
2023-03-07 22:39:58 +0530

    alternative approach and done

--- 07-slider/final/src/index.js
+++ 07-slider/final/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Alternative from './alternative';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Alternative />
   </React.StrictMode>
 );
