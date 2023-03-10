5e1a431646fbb5c4f9845a3c87d41358e6f3dc4b
smajaas <smajaas@gmail.com>
2023-03-10 20:20:09 +0530

    hex single color

--- 09-color-generator/final/src/App.js
+++ 09-color-generator/final/src/App.js
@@ -12,6 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
       console.log(colors);
     } catch (error) {
       setError(true);
@@ -36,7 +37,16 @@ function App() {
         </form>
       </section>
       <section className='colors'>
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
